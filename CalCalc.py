4f81f4595e0bf83be2361e4d793ed5f606e0ca79
Matar Haller <matar@berkeley.edu>
2012-09-12 21:21:02 -0700

    CalCalc module works

--- CalCalc.py
+++ CalCalc.py
@@ -1,20 +1,23 @@
-""" Write a module called ‘CalCalc’, with a method called ‘calculate’ that evaluates any string passed to it, and can be used from either the command line (using argparse with reasonable flags) or imported within Python. Feel free to use something like eval(), but be aware of some of the nasty things it can do, and make sure it doesn’t have too much power: http://nedbatchelder.com/blog/201206/eval_really_is_dangerous.html
-EXAMPLE:
-$ python CalCalc.py -s '34*28'
-$  952
----- AND, from within Python ----
->>> from CalCalc import calculate
->>> calculate('34*20')
->>> 952
-"""
 import argparse
 import urllib2
 from xml.etree import ElementTree as etree
-
+import pdb
 
 def calculate(inputstr):
-	"""Evaluates any string passed
+	"""Write a module called 'CalCalc' with a method 'calculate'
+	that evaluates any string passed to it, 
+	and can be used from either the command line (using argparse with reasonable flags) 
+	or imported within Python. 
 	
+	EXAMPLE:
+	$ python CalCalc.py -s '34*28'
+	$  952
+	---- AND, from within Python ----
+	>>> from CalCalc import calculate
+	>>> calculate('34*20')
+	>>> 952
+	
+	calculate evaluates any string passed
 	EX: calculate('34*90') returns 952"""
 	
 	inputstr = inputstr.split()
@@ -26,22 +29,23 @@ def calculate(inputstr):
 	site = urllib2.urlopen(url)
 	s = site.read()
 	site.close()	
-	
+
 	stree = etree.fromstring(s)
+
 	xmldict = dict()
 	for x in stree.findall('pod'):
 		for y in x.findall('subpod'):
 			for z in y.findall('plaintext'):
-			 	xmldict.update({x.get('title') : z.text})
-	
-	
-
-if name == "__main__":
-	parser = argparse.ArgumentParser(description = "CalCalc from command line")
-	parser.add_argument('-s', action = 'store', dest = 'inputstring', help = "input string to query")
-	
-	args = parser.parse_args()
-	
-	ans = calculate(args)
-	
+		 			xmldict.update({x.get('id') : z.text})
+	try:
+		ans = xmldict['Result']
+	except:
+ 		ans = "Wolfram Alpha doesn't know the answer. Please try a different question"
 	return ans
+	
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description = "CalCalc from command line")
+	parser.add_argument('-s', action = 'store', dest = 'inputstring', help = "input string to query")	
+	args = parser.parse_args()
+	print calculate(args.inputstring)
+	
