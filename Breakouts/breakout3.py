d8002dbf79a22c1f868e974fbcdefcd6cd9c3e7a
Matar Haller <matar@berkeley.edu>
2012-09-29 13:12:45 -0700

    submit HW3

---
+++ Breakouts/breakout3.py
@@ -0,0 +1,5 @@
+cards = np.zeros((52,),dtype = [('card','s1'),('suit','s1'),('value',.4) ])
+
+suits = ['c','s','h','d']
+cardtype = ['A','J','Q','K', range(1:10)]
+if type(cardtype) == str:
---
+++ HW3/HW3.py
@@ -0,0 +1,92 @@
+from __future__ import division
+import scipy.stats as stats
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+def rejsampler1d(target,ref,numsamples):
+	"""
+	Inputs: functional form of  target distribution, 
+			reference distribution in the form of a scipy.stats object (e.g. scipy.stats.norm(0,1))
+			number of samples desired. 
+	Output: the samples, the value of M, and the proportion of samples that was accepted.
+	"""
+	samples = list()
+	tries = 0
+	M = findM(target,ref)
+
+	while len(samples) < numsamples:
+		xc = ref.rvs()
+		u = stats.uniform.rvs()	
+		if (u < (target(xc)/(M*ref.pdf(xc)))):
+			samples.append(xc)
+		tries +=1
+
+	successrate = numsamples/tries
+	return samples, M, successrate
+
+
+def findM(target,ref):
+	x = np.linspace(-6,6)
+	return max(ref.pdf(x)/target(x))
+
+
+def mytargetfunc(x):
+	return (0.7**((abs(x**2))))/.6
+
+
+def answer_hw():
+	##question a
+	print "rejsampler1d defined"
+
+	##question b
+	ref = stats.cauchy(0,1)
+	target = stats.laplace(0,1).pdf
+	numsamples = 1000
+	samples, M, successrate = rejsampler1d(target,ref,numsamples)
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	n,bins,patches = ax.hist(samples,100,alpha = 0.75,normed = True)
+	actual = stats.laplace.pdf(np.linspace(-6,6,100))
+	ax.plot(np.linspace(-6,6,100),actual,'r')
+
+	KStest = stats.kstest(samples,'laplace',(0,1))
+	print "Kolmogorov-Smirnov test statistic : %f \np-value : %f " %(KStest)
+	if KStest[1]>=0.05:
+		keyword = 'not'
+	else:
+		keyword = ""
+	print "samples are %s from laplace(0,1) distribution" %(keyword)
+
+	##question c
+	ref = stats.t(2)
+	target = stats.laplace(0,1).pdf
+	numsamples = 1000
+	samples, M, successrate_student = rejsampler1d(target,ref,numsamples)
+	if successrate_student < successrate:
+		keyword = 'better'
+	else:
+		keyword = 'worse'
+	print "acceptance rate is %f \n" %(successrate)
+	print "using student's t distribution for reference is %s than using a cauchy distribution" %(keyword)
+
+	##question d
+	ref = stats.norm(0,2)
+	target = mytargetfunc
+	numsamples = 5000
+	samples, M, successrate = rejsampler1d(target,ref,numsamples)
+	#plot figure
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	n,bins,patches = ax.hist(samples,100,alpha = 0.75,normed = True)
+	actual = target(np.linspace(-6,6,100))
+	ax.plot(linspace(-6,6,100),actual,'r')
+
+
+
+
+
+
+
+
+
