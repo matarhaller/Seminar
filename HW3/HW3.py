26fd05b538fe516bee949234341048d68da6a676
Matar Haller <matar@berkeley.edu>
2012-09-29 16:50:31 -0700

    actual HW3 submit

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -32,23 +32,31 @@ def findM(target,ref):
 
 
 def mytargetfunc(x):
-	return (0.7**((abs(x**2))))/.6
+	return (0.9**((abs(x**2))))/.6
 
 
 def answer_hw():
 	##question a
-	print "rejsampler1d defined"
+	print '-'*40
+	print "question a : rejsampler1d defined"
+	print '-'*40
+	
 
 	##question b
+	print '-'*40
+	print "question b: cauchy as reference for laplace"
+	print '-'*40
+
 	ref = stats.cauchy(0,1)
 	target = stats.laplace(0,1).pdf
 	numsamples = 1000
 	samples, M, successrate = rejsampler1d(target,ref,numsamples)
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
-	n,bins,patches = ax.hist(samples,100,alpha = 0.75,normed = True)
+	n,bins,patches = ax.hist(samples,100,alpha = 0.75,normed = True,label='Laplace distribution')
 	actual = stats.laplace.pdf(np.linspace(-6,6,100))
-	ax.plot(np.linspace(-6,6,100),actual,'r')
+	ax.plot(np.linspace(-6,6,100),actual,'r',label ='Cauchy distribution')
+	plt.legend()
 
 	KStest = stats.kstest(samples,'laplace',(0,1))
 	print "Kolmogorov-Smirnov test statistic : %f \np-value : %f " %(KStest)
@@ -59,6 +67,10 @@ def answer_hw():
 	print "samples are %s from laplace(0,1) distribution" %(keyword)
 
 	##question c
+	print '-'*40
+	print "question c: t distribution with df =2 as reference for laplace"
+	print '-'*40
+	
 	ref = stats.t(2)
 	target = stats.laplace(0,1).pdf
 	numsamples = 1000
@@ -71,6 +83,9 @@ def answer_hw():
 	print "using student's t distribution for reference is %s than using a cauchy distribution" %(keyword)
 
 	##question d
+	print '-'*40
+	print "question d: novel continuous distribution"
+	print '-'*40
 	ref = stats.norm(0,2)
 	target = mytargetfunc
 	numsamples = 5000
@@ -78,9 +93,10 @@ def answer_hw():
 	#plot figure
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
-	n,bins,patches = ax.hist(samples,100,alpha = 0.75,normed = True)
+	n,bins,patches = ax.hist(samples,100,alpha = 0.75,normed = True,label = 'target function')
 	actual = target(np.linspace(-6,6,100))
-	ax.plot(linspace(-6,6,100),actual,'r')
+	ax.plot(np.linspace(-6,6,100),actual,'r',label = 'normal distribution')
+	plt.legend()
 
 
 
