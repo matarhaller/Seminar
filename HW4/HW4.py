00ff2b9da7fe37024339e6c85fc10232dbbd520f
Matar Haller <matar@berkeley.edu>
2012-10-07 10:46:53 -0700

    HW4 submit

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -3,62 +3,119 @@ import pylab
 import scipy
 import numpy as np
 import matplotlib.pyplot as plt
+import urllib2
+from bs4 import BeautifulSoup
+from matplotlib import mlab
+import argparse
 
-#open file
-wf = aifc.open('/Users/matar/Documents/Courses/python-seminar/Homeworks/homework4_data/F4_CathedralOrgan.aif', 'rb')
+def get_soundfile(filename,dplot = 1):
+	"""
+	reads in the acii file, optionally plots
+	"""
+	#open file
+	wf = aifc.open(filename, 'rb')
 
-#read data
-data = wf.readframes(wf.getnframes())
-srate = wf.getframerate()
+	#read data
+	data = wf.readframes(wf.getnframes())
+	srate = wf.getframerate()
 
-#close file
-wf.close()
+	#close file
+	wf.close()
 
-#format data, time
-data_int = pylab.fromstring(data,dtype=np.int32)
-n = data_int.size
-time = np.arange(n)/float(srate)
-time = time*1000 #ms
+	#format data, time
+	data_int = pylab.fromstring(data,dtype=np.int32)
+	n = data_int.size
+	time = np.arange(n)/float(srate)
+	time = time*1000 #ms
 
-#plot sound file
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-ax1.plot(time,data_int)
-ax1.set_xlim(min(time), max(time))
-ax1.set_xlabel("Time [ms]")
-ax1.set_ylabel("Amplitude")
+	#plot sound file
+	if dplot:
+		fig = plt.figure()
+		ax1 = fig.add_subplot(1,1,1)
+		ax1.plot(time,data_int)
+		ax1.set_xlim(min(time), max(time))
+		ax1.set_xlabel("Time [ms]")
+		ax1.set_ylabel("Amplitude")
 
-##fft
-npoints = np.ceil((n+1)/2.0) #number of unique timepoints (single sided)
-data_fft = scipy.fft(data_int) #compute fft
-data_fft = data_fft[0:npoints]
-Y = abs(data_fft)
-Y = Y/float(n) #normalize by length
+	return data_int, srate
 
-freqs = np.arange(0, npoints, 1.0) * (srate / float(n))
+def get_peakfreq(data,srate,dplot = 1):
+	##fft
+	n = data.size
+	npoints = np.ceil((n+1)/2.0) #number of unique timepoints (single sided)
+	data_fft = scipy.fft(data) #compute fft
+	data_fft = data_fft[0:npoints]
+	Y = abs(data_fft)
+	Y = Y/float(n) #normalize by length
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-ax1.plot(freqs, abs(Y),color = 'r')
+	##find peak freq
+	freqs = np.arange(0, npoints, 1.0) * (srate / float(n))
+	maxfreq = freqs[mlab.find(Y==max(Y))][0]
 
-#calculate power spectral density
-Y = Y**2 #square for power
+	if dplot:
+		#plot fft
+		fig = plt.figure()
+		ax1 = fig.add_subplot(1,1,1)
+		#ax1.plot(freqs/1000, 10*np.log10(Y), color='g')
+		ax1.plot(freqs,Y,color='m')
+		ax1.set_xlabel('Frequency (kHz)')
+		ax1.set_ylabel('Power (dB)')
+		plt.show()
 
-if n % 2 > 0: #odd fft
-	Y[1:len(Y) - 1] = Y[1:len(Y)]*2
-else:
-	Y[1:len(Y)-1] = Y[1:len(Y)-1]*2
+	##find fundamental freq
+	return maxfreq
+
+def make_notedict():
+	#lookup table
+	response = urllib2.urlopen("http://www.contrabass.com/pages/frequency.html")
+	#response = urllib2.urlopen('http://www.phy.mtu.edu/~suits/notefreqs.html')
+	html = response.read()
+	response.close()
+
+	page = BeautifulSoup(html)
+
+	header = ["note","frequency","comments","lowest note", "highest note"]
+	notedict = {}
+
+	for key in header:
+		notedict[key] = []
+
+	for tablerow in [x.findAll("td") for x in page.find('tbody').find_all('tr')[2:]]:
+		datalist = [val.get_text() for val in tablerow]
+		for idx, key in enumerate (header):
+			try:
+				notedict[key].append(float(datalist[idx]))
+			except:
+				notedict[key].append(datalist[idx])
+
+	return notedict
+
+def get_note(freq):
+	notedict = make_notedict()
+
+	freqs = notedict['frequency']
+
+	diffs = abs(freqs-freq)
+
+	idx = mlab.find(diffs== min(diffs))
+
+	return notedict['note'][idx]
 
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-ax1.plot(freqs/1000, 10*np.log10(Y), color='g')
-ax1.set_xlabel('Frequency (kHz)')
-ax1.set_ylabel('Power (dB)')
-plt.show()
+def answer_hw(filename,dplot):
+	print "-"*40
+	print "question 2 - notes"
+
+	data, srate = get_soundfile(filename,dplot)
+	maxfreq = get_peakfreq(data,srate,dplot)
+	note = get_note(maxfreq)
+	print "the note is" + note
+	print "harmonics not accounted for"
 
 
-pwr,fq = mlab.psd(data_int,Fs=srate)
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-ax1.plot(fq,pwr)
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description = "HW4 from the command line")
+	parser.add_argument('-s', action = 'store', dest = 'inputstr', help = 'full path to music file')	
+	parser.add_argument('-i', action = 'store', dest = 'dplot', help = 'whether or not to plot')
+	args = parser.parse_args()
+	print answer_hw(args.inputstr, args.dplot)
--- HW4/README.txt
+++ HW4/README.txt
@@ -1,4 +1,11 @@
 HW4 has 3 files - 
 HW4_client.py and HW4_server.py are for q1
 	note that HW4_client should be run from within the HW4/ directory becuase it references a file that is in the HW2 directory (../HW2/) and I didn't give the full path.
+
 HW4.py is for q2
+*should* be able to run it from the command line with the filename and whether or not to plot
+run HW4.py -s "1.aif", -i 1
+
+I was having problems with the file locations, so I just moved 1 file into the HW4 directory as an example. (for some reason giving the full path to the music file didn't work for me)
+
+this doesn't account for harmonics, so it only provides the frequency with the maximum power
