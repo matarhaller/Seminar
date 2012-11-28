897299dda4b415876bbbdb980b780d3343dbfa25
Matar Haller <matar@berkeley.edu>
2012-11-28 12:13:23 -0800

    added logging function, took CAR outside of Subject class

--- FinalProject/subj_globals.py
+++ FinalProject/subj_globals.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 import os
 import math
 import scipy.io
+import cython
 
 class Subject():
 	"""
 	Makes an Subject object with all of the data parameters and raw data.
 	Includes methods for manipulating the data.
 	"""
+
 	def __init__(self, subj, block, elecs, ANsrate, srate, gdat, SJdir, Events):
 		#initialize variables
 		self.subj = subj		#subject name (ie 'ST22')
@@ -19,7 +21,8 @@ class Subject():
 		self.srate = srate		#digital sampling rate
 		self.gdat = gdat 		#raw data matrix elecs x tmpts (numpy array)
 		self.SJdir = SJdir 		#subject directory (/DATA/Stanford/Subjs/)
-		self.Events = Events    #dictionary with fields onsets,offsets, RTs...
+		self.Events = Events    #Events class with timing/behavior info.
+
 
 		#create analysis and data folders (DO I NEED BOTH?)
 		self.DTdir = os.path.join(self.SJdir, self.subj, 'data', self.block)
@@ -32,64 +35,16 @@ class Subject():
 		if not os.path.isdir(self.ANdir):
 			os.makedirs(self.ANdir)
 			print 'making ' + self.ANdir
-		
-	def create_CAR(self, grouping): #need to parallelize with cython. 
-	#too big to have as method inside a class?
-	#to make less data - should overwrite the raw gdat with gdat_CAR, orig gdat will stay (outside of class)
-		""" 
-		Create common average reference data matrix, add to class.
-		Calcuates CAR from only good electrodes, removes CAR from all elecs
-		INPUT:
-			grouping -  size of groups to average 
-						(noise can come in banks of 16 because of preamp)
-						if noise isn't grouped, grouping = number of electrodes
-		"""
-		numelecs = min(self.gdat.shape) # number of electrodes
-		chRank = np.zeros(numelecs) 
-		chRank[self.elecs] = 1 # which electrodes are good
-		Ngroups = math.ceil(self.elecs[-1]/grouping)
 
-		# make lookup table of electrode groupings 
-		# according to how plugged in on the preamp
-		groups =np.array([x*np.ones(grouping) for x in np.arange(Ngroups)])
-		groups = groups.flatten()
+		#logfile creation
+		self.logfile = os.path.join(ANdir 'logfile.log')
+		self.logit('created %s - %s' %(self.subj, self.block))
 
-		# preallocate CAR
-		gdat_CAR = self.gdat.copy() # numelecs X tmpts
-		CAR = np.zeros((Ngroups, max(self.gdat.shape))) # Ngroups X tmpts
-		CAR_all = np.zeros(max(self.gdat.shape)) # 1 X tmepts
-
-		# subtract the mean from each electrode (including bad)
-		# then sum gdat by group only for valid electrodes
-		for e in self.elecs:
-			gdat_CAR[e,:] = self.gdat[e,:] - np.mean(self.gdat[e,:]) 
-			if chRank[e]: # valid elec
-				CAR[groups[e],:] = CAR[groups[e],:] + gdat_CAR[e,:]
-
-		# divide by number of valid elecs in each group (to get CAR per group)
-		for cnt in np.arange(Ngroups):
-			if np.sum(chRank[np.flatnonzero(groups == cnt)]): 
-				#only if there is at least 1 valid elec in that group
-				CAR[cnt,:] = np.divide(CAR[cnt,:], np.sum(chRank[np.flatnonzero(groups == cnt)]))
-
-		# remove the relevant group CAR from each elec
-		for e in np.arange(numelecs):
-			gdat_CAR[e,:] = gdat_CAR[e,:] - CAR[groups[e],:]
-			if chRank[e]:
-				CAR_all = CAR_all + gdat_CAR[e,:] #sum CAR for valid elecs
-
-		# calculate CAR for all elecs
-		CAR_all = np.divide(CAR_all, len(self.elecs))
-
-		# add grouped CAR to class
-		self.gdat_CAR_group = gdat_CAR 
-
-		# remove total CAR for all electrodes (ungrouped)
-		for e in np.arange(numelecs):
-			gdat_CAR[e,:] = gdat_CAR[e,:] - CAR_all
-
-		# add ungrouped CAR to class
-		self.gdat_CAR = gdat_CAR
+	def logit(self, message):
+		logf = open(self.logfile, "a")
+		logf.write('[%s] %s' % (datetime.datetime.now(), message))
+		logf.flush()
+		logf.close()
 
 	def resample(self, srate_new=1000):
 		"""
@@ -98,7 +53,78 @@ class Subject():
 		#find rational fraction for resampling
 		p, q = (srate_new / self.srate).as_integer_ratio()
 
-		for e in np.arange(min(self.gdat.shape)):
+		# NEED HELP installing upfirdn
+
+		self.logit('resampled to %i' %(srate_new))
+
+
+	def save(self):
+		""" saves object to file to be read later"""
+		filename = os.path.join(self.DTdir, 'alldata.pic')
+		with open (filename, 'wb') as output:
+			cPickle.dump(self, output, cPickle.HIGHEST_PROTOCOL)
+
+		self.logit('saved to %s' %(DTdir))
+
+		
+def create_CAR(dataobj, grouping): 
+#too big to have as method inside a class?
+#to make less data - should overwrite the raw gdat with gdat_CAR, orig gdat will stay (outside of class)??
+	""" 
+	Create common average reference data matrix, add to class.
+	Calcuates CAR from only good electrodes, removes CAR from all elecs
+	INPUT:
+		grouping -  size of groups to average 
+					(noise can come in banks of 16 because of preamp)
+					if noise isn't grouped, grouping = number of electrodes
+	"""
+	numelecs = min(dataobj.gdat.shape) # number of electrodes
+	chRank = np.zeros(numelecs) 
+	chRank[dataobj.elecs] = 1 # which electrodes are good
+	Ngroups = math.ceil(dataobj.elecs[-1]/grouping)
+
+	# make lookup table of electrode groupings 
+	# according to how plugged in on the preamp
+	groups =np.array([x*np.ones(grouping) for x in np.arange(Ngroups)])
+	groups = groups.flatten()
+
+	# preallocate CAR
+	gdat_CAR = self.gdat # numelecs X tmpts DONT DO COPY BC TOO MUCH DATA
+	CAR = np.zeros((Ngroups, max(dataobj.gdat.shape))) # Ngroups X tmpts
+	CAR_all = np.zeros(max(dataobj.gdat.shape)) # 1 X tmepts
+
+	# subtract the mean from each electrode (including bad)
+	# then sum gdat by group only for valid electrodes
+	for e in self.elecs:
+		gdat_CAR[e,:] = self.gdat[e,:] - np.mean(self.gdat[e,:]) 
+		if chRank[e]: # valid elec
+			CAR[groups[e],:] = CAR[groups[e],:] + gdat_CAR[e,:]
+
+	# divide by number of valid elecs in each group (to get CAR per group)
+	for cnt in np.arange(Ngroups):
+		if np.sum(chRank[np.flatnonzero(groups == cnt)]): 
+			#only if there is at least 1 valid elec in that group
+			CAR[cnt,:] = np.divide(CAR[cnt,:], np.sum(chRank[np.flatnonzero(groups == cnt)]))
+
+	# remove the relevant group CAR from each elec
+	for e in np.arange(numelecs):
+		gdat_CAR[e,:] = gdat_CAR[e,:] - CAR[groups[e],:]
+		if chRank[e]:
+			CAR_all = CAR_all + gdat_CAR[e,:] #sum CAR for valid elecs
+
+	# calculate CAR for all elecs
+	CAR_all = np.divide(CAR_all, len(self.elecs))
+
+	# add grouped CAR to class
+	self.gdat_CAR_group = gdat_CAR 
+
+	# remove total CAR for all electrodes (ungrouped)
+	for e in np.arange(numelecs):
+		gdat_CAR[e,:] = gdat_CAR[e,:] - CAR_all
+
+	# add ungrouped CAR to class
+	self.gdat_CAR = gdat_CAR
+
 
 
 
