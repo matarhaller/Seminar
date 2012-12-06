f18f78c661cee828637a58e1eca77c6b071f7888
Matar Haller <matar@berkeley.edu>
2012-12-06 13:37:38 -0800

    fixed setup.py - CARcython compiled

--- FinalProject/CARcython.pyx
+++ FinalProject/CARcython.pyx
@@ -1,43 +1,39 @@
 #cython: cdivision = True
 import cython
-cimport numpy as cnp
+cimport numpy as np
 import numpy as np
+import math
+import h5py
 
 @cython.boundscheck(False)
 @cython.wraparound(False) #cancel ability to do -1 for last element
-def CARcython(cnp.ndarray[cnp.float64, ndim=1] elecs, cnp.ndarray[cnp.float64, ndim =1] chRank, cnp.ndarray[cnp.float64, ndim = 1] groups, int Ngroups, int numelecs, char gdatfilename):
+def CARcython(np.ndarray elecs, np.ndarray chRank, np.ndarray groups, int Ngroups, int numelecs, char gdatfilename):
 	
 	f = h5py.File(gdatfilename,'a')
 	gdat = f['gdat']
 
-	cdef size_t e, cnt
-	cdef cnp.ndarray[cnp.float64, ndim = 2] gdat_CAR = gdat
-	cdef cnp.ndarray[cnp.float64, ndim = 2] CAR = np.zeros(Ngroups, max(gdat.shape))
-	cdef cnp.ndarray[cnp.float64, ndim = 1] CAR_all = np.zeros(max(gdat.shape))
+	cdef int e, cnt
+	cdef np.ndarray gdat_CAR = gdat
+	cdef np.ndarray CAR = np.zeros(Ngroups, max(gdat.shape))
+	cdef np.ndarray CAR_all = np.zeros(max(gdat.shape))
 
-	#numelecs = min(gdat.shape) # total number of electrodes
-	#chRank = np.zeros(numelecs) 
-	#chRank[dataobj.elecs] = 1 # which electrodes are good
-	#Ngroups = math.ceil(dataobj.elecs[-1]/grouping)
+	for e in elecs:
+		gdat_CAR[e,:] = gdat[e,:] - np.mean(gdat[e,:])
+		if chRank[e]:
+			CAR[groups[e],:] = CAR[groups[e],:] + gdat_CAR[e,:]
 
-	with nogil:
-		for e in elecs:
-			gdat_CAR[e,:] = gdat[e,:] - np.mean(gdat[e,:])
-			if chRank[e]:
-				CAR[groups[e],:] = CAR[groups[e],:] + gdat_CAR[e,:]
+	for cnt in np.prange(Ngroups):
+		if np.sum(chRank[np.flatnonzero(groups == cnt)]):
+			CAR[cnt,:] = np.divide(CAR[cnt,:], np.sum(chRank[np.flatnonzero(groups == cnt)]))
 
-		for cnt in prange(Ngroups):
-			if np.sum(chRank[np.flatnonzero(groups == cnt)]):
-				CAR[cnt,:] = np.divide(CAR[cnt,:], np.sum(chRank[np.flatnonzero(groups == cnt)]))
+	for e in np.prange(numelecs):
+		gdat_CAR[e,:] = gdat_CAR[e,:] - CAR[groups[e],:]
+		if chRank[e]:
+			CAR_all = CAR_all + gdat_CAR[e,:]
 
-		for e in prange(numelecs):
-			gdat_CAR[e,:] = gdat_CAR[e,:] - CAR[groups[e],:]
-			if chRank[e]:
-				CAR_all = CAR_all + gdat_CAR[e,:]
-
-		CAR_all = np.divide(CAR_all, len(elecs))
+	CAR_all = np.divide(CAR_all, len(elecs))
 
 	return CAR_all, gdat_CAR, CAR
 
-	# pull three loops out into sep functions - only they will be in cython and compiled. write them in a separate file completely - in setup.py run them and compile them separately. can just feed it in the file object to the gdat or the filepath. in cython code can open the db and do for item in db. from comiled cython code import blah, on this file object/path do whatever.
-	#write cython code (code.pyx) where do cdef to define function, run cython on it one time (definied in setup.py) - makes code.co file - then in here (python file) from code import function. now function is a python function (reads it in from code.so) or look at numexpr.
+# pull three loops out into sep functions - only they will be in cython and compiled. write them in a separate file completely - in setup.py run them and compile them separately. can just feed it in the file object to the gdat or the filepath. in cython code can open the db and do for item in db. from comiled cython code import blah, on this file object/path do whatever.
+# write cython code (code.pyx) where do cdef to define function, run cython on it one time (definied in setup.py) - makes code.co file - then in here (python file) from code import function. now function is a python function (reads it in from code.so) or look at numexpr.
---
+++ FinalProject/setup.py
@@ -0,0 +1,10 @@
+import numpy as np
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+setup(
+	cmdclass = {'build_ext' : build_ext},
+	ext_modules = [
+		Extension("CARcython", ["CARcython.pyx"], include_dirs = [np.get_include()])
+		])
